6bf127d3b72c48ec8b4dfc76815ffa4ee780ee5b
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-11-02 20:17:56 -0500

    refactor

--- test/enum_test.exs
+++ test/enum_test.exs
@@ -1,36 +1,38 @@
 defmodule EnumTest do
   use ExUnit.Case
 
+  import Enum
+
   test "empty?" do
-    Enum.empty?([])
+    empty?([])
   end
 
   test "chunk(list,n) splits list into multiple lists with n elements each" do
-    chunks = Enum.chunk(list_12, 6)
-    assert 2 == Enum.count(chunks)
-    assert [0,1,2,3,4,5] == Enum.at(chunks, 0)
+    chunks = chunk(list_12, 6)
+    assert 2 == count(chunks)
+    assert [0,1,2,3,4,5] == at(chunks, 0)
   end
 
   test "chunk(list,n,step) starts each new chunked list by stepping \"step\"s into the original list. The chunked lists can have overlapping members" do
-    chunks = Enum.chunk(list_with_6, 2, 2)
+    chunks = chunk(list_with_6, 2, 2)
     assert [[0,1], [2,3], [4,5]] == chunks
   end
 
   test "chunk(list,n,step,pad) fills in the last chunk from pad, if necessary" do
-    chunks = Enum.chunk(list_12, 5,5,[:a, :b, :c, :d])
-    assert Enum.at(chunks,2) == [10, 11, :a, :b, :c]
+    chunks = chunk(list_12, 5,5,[:a, :b, :c, :d])
+    assert at(chunks,2) == [10, 11, :a, :b, :c]
   end
   
   test "chunk(list,n,step,nil) will build chunks only up to what is defined by n and step. original items my not appear in new chunks " do
-    chunks = Enum.chunk([:a, :b, :c, :d, :e, :f], 4,4)
-    assert 1 == Enum.count(chunks)
-    assert [:a,:b,:c,:d] == Enum.at(chunks,0)
+    chunks = chunk([:a, :b, :c, :d, :e, :f], 4,4)
+    assert 1 == count(chunks)
+    assert [:a,:b,:c,:d] == at(chunks,0)
   end
 
   test "chunk(list,n,step,[]) may build the last chunk with fewer items than the previous chunks" do
-    chunks = Enum.chunk([:a, :b, :c, :d, :e, :f], 4,4,[])
-    assert 2 == Enum.count(chunks)
-    assert [:e,:f] == Enum.at(chunks,1)
+    chunks = chunk([:a, :b, :c, :d, :e, :f], 4,4,[])
+    assert 2 == count(chunks)
+    assert [:e,:f] == at(chunks,1)
   end
 
 
--- test/option_parser_test.exs
+++ test/option_parser_test.exs
@@ -1,38 +1,40 @@
 defmodule OptionParserTest do
   use ExUnit.Case
 
+  import OptionParser
+
   test "parse_args parses argv into a keywords list" do
-    parsed = OptionParser.parse(["--foo", "--bar"])
+    parsed = parse(["--foo", "--bar"])
     kw = elem(parsed, 0)
     assert kw == [{:foo, true}, {:bar, true}]
     assert kw == [foo: true, bar: true]   # keyword syntactic sugar
   end
 
   test "parse_args returns a 3 element tuple" do
-    parsed = OptionParser.parse(["--name=foo"])
+    parsed = parse(["--name=foo"])
     assert tuple_size(parsed) == 3
   end
 
   test "given parse_args returns a tuple, the first element is the keyword element" do
-    parsed =  OptionParser.parse(["--foo", "--bar"])
+    parsed =  parse(["--foo", "--bar"])
     kw = elem(parsed, 0)
     assert kw == [{:foo, true}, {:bar, true}]
   end
 
   test "given parse_args returns a tuple, the second element is the remaining arguments" do
-    parsed =  OptionParser.parse(["--verbose=y", "path/and/file"])
+    parsed =  parse(["--verbose=y", "path/and/file"])
     kw = elem(parsed, 1)
     assert kw == ["path/and/file"]
   end
 
   test "given parse_args returns a tuple, the third element is errors" do
-    parsed = OptionParser.parse(["--verbose","--n=yes"])
+    parsed = parse(["--verbose","--n=yes"])
     kw = elem(parsed,2)
     assert kw != nil
   end
 
   test "use 'switches' to parse an arg as a boolean" do
-    parsed = OptionParser.parse(["--verbose"], switches: [verbose: :boolean])
+    parsed = parse(["--verbose"], switches: [verbose: :boolean])
     assert (
     case parsed do
       {[verbose: true], _, _} -> true
@@ -42,7 +44,7 @@ defmodule OptionParserTest do
   end
 
   test "switching to boolean is the default when nothing follows the arg" do
-    parsed = OptionParser.parse(["--verbose"])
+    parsed = parse(["--verbose"])
     assert (
     case parsed do
       {[verbose: true], _, _} -> true
@@ -52,7 +54,7 @@ defmodule OptionParserTest do
   end
 
   test "arg values are cast to strings by default" do
-    parsed = OptionParser.parse(["--verbose=42"])
+    parsed = parse(["--verbose=42"])
     assert (
     case parsed do
       {[verbose: 42], _, _} -> false
@@ -63,7 +65,7 @@ defmodule OptionParserTest do
   end
 
   test "arg values can be switched to integer" do
-    parsed = OptionParser.parse(["--n=42"], switches: [n: :integer])
+    parsed = parse(["--n=42"], switches: [n: :integer])
     assert (
     case parsed do
       {[n: "42"], _, _} -> false
@@ -74,7 +76,7 @@ defmodule OptionParserTest do
   end
 
   test "= signs are optional" do
-    parsed = OptionParser.parse(["--n", "42"], switches: [n: :integer])
+    parsed = parse(["--n", "42"], switches: [n: :integer])
     assert (
     case parsed do
       {[n: "42"], _, _} -> false
@@ -85,7 +87,7 @@ defmodule OptionParserTest do
   end
 
   test "aliases ....." do
-    parsed = OptionParser.parse(["-v"], aliases: [v: :verbose])
+    parsed = parse(["-v"], aliases: [v: :verbose])
     assert (
     case parsed do
       {[verbose: true], _, _} -> true
